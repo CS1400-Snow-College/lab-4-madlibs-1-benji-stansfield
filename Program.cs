8170f2461456a41f002e3e7ec1672ca2da9b4ecb
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-02-16 14:14:50 -0700

    Added a greeting

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿//Benji Stansfield, 02-16-25, Lab 4 "Madlibs 1"
-Console.WriteLine("Hello, World!");
+
+Console.Clear();
+
+//Greeting
+Console.WriteLine(@"This is Mad Libs. When you are done providing the requested words I will build them into a funny story for you.
+Press any key to continue.");
+Console.ReadKey(true);
+Console.WriteLine(@"
+Please enter the requested word types:");
